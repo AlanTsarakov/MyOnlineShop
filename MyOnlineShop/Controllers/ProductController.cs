351099e99abaac35e2171579a93d2ef5c464cabd
Алан <@DESKTOP-SFTGPJO>
2025-10-08 22:21:07 +0300

    Добавил модель и представление для Product.

---
+++ MyOnlineShop/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyOnlineShop.Repositories;
+
+namespace MyOnlineShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: ProductController
+        public ActionResult Index(int id)
+        {
+            var product = ProductsRepository.TryGetById(id);
+            return View(product);
+        }
+    }
+}
--- MyOnlineShop/Repositories/ProductsRepository.cs
+++ MyOnlineShop/Repositories/ProductsRepository.cs
@@ -5,7 +5,7 @@ namespace MyOnlineShop.Repositories
     public static class ProductsRepository
     {
         private static int _instanceCounter = 0;
-        static List<Product> products = new List<Product> { new Product(++_instanceCounter, "Хлеб", 20), new Product(++_instanceCounter, "Масло", 800), new Product(++_instanceCounter, "Колбама", 200) };
+        static List<Product> products = new List<Product> { new Product(++_instanceCounter, "Хлеб", 20), new Product(++_instanceCounter, "Масло", 800), new Product(++_instanceCounter, "Колбаcа", 200) };
 
         public static List<Product> GetAll()
         {
@@ -15,6 +15,18 @@ namespace MyOnlineShop.Repositories
         public static void Add(string name, int price)
         {
             products.Add(new Product(++_instanceCounter, name, price));
+        }
+
+        public static Product? TryGetById(int id) 
+        {
+            try
+            {
+                return products[id - 1];
+            }
+            catch
+            {
+                return null;
+            }
         }
     }
 }
